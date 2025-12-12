169291aadb4507e201d853b61fc14bd8ecd58160
Jonathan Royle <jono.royle@gmail.com>
2025-12-12 15:19:24 +0100

    Initial controller commit

---
+++ API/AppointmentAPI/Controllers/AppointmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentAPI.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
