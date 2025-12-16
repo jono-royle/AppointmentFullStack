7cfc143bb3e39b850c0e8cbbb1503351fc1edc4c
Jonathan Royle <jono.royle@gmail.com>
2025-12-16 12:06:46 +0100

    Build and deploy instructions

--- Frontend/react-appointments/vite.config.ts
+++ Frontend/react-appointments/vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost:7263",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL ?? "https://localhost:7263",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
-
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # AppointmentFullStack
 
+Submission for Senior Full-Stack Engineer Case Study for Jonathan Royle
+
+Setup and run instructions:
+
+Backend - Navigate to AppointmentFullStack\API\AppointmentAPI and use command 'dotnet run'
+The backend will now be listening on a local HTTP or HTTPS port shown in the console output, and the swagger can be viewed on localhost:{port}/swagger
+
+Frontend - Navigate to AppointmentFullStack\Frontend\react-appointments. Create or update the file '.env.local' in this directory and set the backend URL in that file as follows:
+VITE_BACKEND_URL=http://localhost:{port}
+or
+VITE_BACKEND_URL=https://localhost:{port}
+Then, run the frontend react site with command 'npm run dev' and open the URL shown in the console
+
